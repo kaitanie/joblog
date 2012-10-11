ddecb739767754d5862697a38d5256f2e2929976
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-10-11 07:49:39 +0300

    Downgrade the nrepl version to something that is actually available in the maven repos

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
 ;;                 [clj-histogram "0.0.1-SNAPSHOT"]
                  [clj-time "0.4.4"]
-                 [org.clojure/tools.nrepl "0.2.0-SNAPSHOT"] ;; "0.2.0-beta9"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]
                  [protobuf "0.6.1"]]
   :plugins [[lein-protobuf "0.2.1"]]
   :main log.core)
