d3259bed33bd9e0cd88cc9ae4c120f48a7f14601
Pekka Kaitaniemi <pekka.kaitaniemi@gmail.com>
2012-10-16 18:36:07 +0300

    Add protocol buffer tests

--- test/log/sgeimporter_test.clj
+++ test/log/sgeimporter_test.clj
@@ -50,6 +50,15 @@ maxvmem      1.475G")
 
 (def parsed-sge-entry-1 {:group "cmsuser04", :ru_msgrcv 0, :account "sge", :ru_nvcsw 112339, :cpu 503.0, :ru_utime 495.0, :iow 0.0, :jobnumber 1315567, :qsub_time {:second 58, :minute 38, :hour 10, :year 2012, :dayname "Tue", :month 10, :day 16}, :ru_ixrss 0, :granted_pe "NONE", :ru_stime 8.0, :ru_msgsnd 0, :ru_inblock 0, :taskid "undefined", :start_time {:second 10, :minute 14, :hour 11, :year 2012, :dayname "Tue", :month 10, :day 16}, :slots 1, :ru_wallclock 675.0, :ru_nivcsw 37414, :ru_idrss 0, :end_time {:second 25, :minute 25, :hour 11, :year 2012, :dayname "Tue", :month 10, :day 16}, :failed "0", :owner "cmsuser04", :hostname "compute-0-27.local", :exit_status 0, :ru_minflt 1056970, :ru_oublock 0, :ru_ismrss 0, :ru_isrss 0, :ru_maxrss 575780, :maxvmem 1.475, :ru_majflt 0, :mem 468.03, :department "cms_users", :ru_nswap 0, :jobname "https___wms317_", :io 0.0, :qname "mgrid", :priority 10, :project "NONE", :ru_nsignals 0})
 
+(def test-parsing-sge-entry-1 (parse-sge-attr-block (line-seq (java.io.BufferedReader. (java.io.StringReader. sge-entry1)))))
+
+(def protobuf-sge-entry-1 (serialize-jobentry test-parsing-sge-entry-1))
+
+(defn is-sge-date? [d]
+  (let [day-keys [:day :dayname :month :year :hour :minute :second]]
+    (and (=  (count (keys d)) (count day-keys))
+         (reduce (fn [a b] (and a b)) (map (fn [x] (not (nil? (d x)))) day-keys)))))
+
 (fact "String \" \" is whitespace" (is-whitespace? " ") => true)
 (fact "Char space is whitespace" (is-whitespace? \space) => true)
 (fact "String \"a\" is not whitespace" (is-whitespace? "a") => false)
@@ -73,4 +82,81 @@ maxvmem      1.475G")
 (fact (parse-sge-attr-line "qname     mgrid") => {:qname "mgrid"})
 (fact (parse-sge-attr-line "qsub_time    Sun Jan  1 00:04:25 2012") => {:qsub_time {:dayname "Sun" :year 2012 :month 1 :day 1 :hour 0 :minute 4 :second 25}})
 
-(fact (parse-sge-attr-block (line-seq (java.io.BufferedReader. (java.io.StringReader. sge-entry1)))) => parsed-sge-entry-1)
+(fact test-parsing-sge-entry-1 => parsed-sge-entry-1)
+(fact (:group test-parsing-sge-entry-1) => (:group parsed-sge-entry-1))
+
+(fact (:group protobuf-sge-entry-1) => (:group parsed-sge-entry-1))
+
+(fact (count (keys protobuf-sge-entry-1)) => (count (keys parsed-sge-entry-1)))
+
+(defn check-field [fname]
+  (do
+    (fact (str "Field " fname " not nil" (not (nil? (fname protobuf-sge-entry-1))) => true))
+    ;; Dates are stored in maps that have different types (clojure.lang.PersistentHashMap vs protobuf.core.PersistentProtocolBufferMap)
+    (if (not (fname #{:qsub_time :start_time :end_time}))
+      (fact (str "Types match") (type (fname protobuf-sge-entry-1)) => (type (fname parsed-sge-entry-1)))
+      (do
+        (fact "Date types" (type (fname parsed-sge-entry-1)) => clojure.lang.PersistentHashMap)
+        (fact "Date types" (type (fname protobuf-sge-entry-1)) => protobuf.core.PersistentProtocolBufferMap)))
+    (fact (str "Checking field " fname) (fname protobuf-sge-entry-1) => (fname parsed-sge-entry-1))))
+
+(check-field :group)
+(check-field :ru_msgrcv)
+(check-field :account)
+(check-field :ru_nvcsw)
+(check-field :cpu)
+(check-field :ru_utime)
+(check-field :iow)
+(check-field :jobnumber)
+
+(fact "qsub_time is an SGE date" (is-sge-date? (:qsub_time protobuf-sge-entry-1)) => true)
+(check-field :qsub_time)
+
+(check-field :ru_ixrss)
+(check-field :granted_pe)
+(check-field :ru_stime)
+(check-field :ru_msgsnd)
+(check-field :ru_inblock)
+(check-field :taskid)
+
+(fact "start_time is an SGE date" (is-sge-date? (:start_time protobuf-sge-entry-1)) => true)
+(check-field :start_time)
+
+(check-field :slots)
+(check-field :ru_wallclock)
+(check-field :ru_nivcsw)
+(check-field :ru_idrss)
+
+(fact "end_time is an SGE date" (is-sge-date? (:end_time protobuf-sge-entry-1)) => true)
+(check-field :end_time)
+
+(check-field :failed)
+(check-field :owner)
+(check-field :hostname)
+(check-field :exit_status)
+(check-field :ru_minflt)
+(check-field :ru_oublock)
+(check-field :ru_ismrss)
+(check-field :ru_isrss)
+(check-field :ru_maxrss)
+(check-field :maxvmem)
+(check-field :ru_majflt)
+
+(fact "Check :mem field type" (type  (:mem protobuf-sge-entry-1)) => (type  (:mem parsed-sge-entry-1)))
+(check-field :mem)
+
+(check-field :department)
+(check-field :ru_nswap)
+(check-field :jobname)
+(check-field :io)
+(check-field :qname)
+(check-field :priority)
+(check-field :project)
+(check-field :ru_nsignals)
+
+
+;;(doseq [k (keys protobuf-sge-entry-1)] (fact "Check field type" (type  (k protobuf-sge-entry-1)) => (type  (k parsed-sge-entry-1))))
+
+
+;; Check the serialized fields
+;;(for [k (keys protobuf-sge-entry-1)] (fact (str "Check the content of field " k) (k protobuf-sge-entry-1) => (k parsed-sge-entry-1)))
